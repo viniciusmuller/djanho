cfc8bc4f704b50bf884707dcdd1e29203d6138dc
Vinícius Müller <vinigm.nho@gmail.com>
2021-08-21 20:32:14 -0300

    First commit

---
+++ flake.nix
@@ -0,0 +1,17 @@
+{
+  description = "Vscode->Vim colorscheme converter";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in
+          rec {
+            devShell = pkgs.mkShell {
+              buildInputs = with pkgs; [ rustc cargo clippy rustfmt ];
+            };
+          }
+    );
+}
